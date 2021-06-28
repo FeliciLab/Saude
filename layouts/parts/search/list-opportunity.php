f61c4eb9882b546b7861847e6202e70d4e9fc972
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2021-06-28 16:06:37 -0300

    #134 🐛 adcionando verificacao com isType

--- layouts/parts/search/list-opportunity.php
+++ layouts/parts/search/list-opportunity.php
@@ -1,20 +1,20 @@
 <header id="opportunity-list-header" class="entity-list-header clearfix" ng-show="data.global.filterEntity == 'opportunity'">
     <div class="clearfix">
-        <div ng-if="data.global.type == 'edital'">
+        <div ng-if="data.global.isType === false">
         <h1><i class="fa fa-file-text" aria-hidden="true"></i></span> Editais e Concursos</h1>
         </br>
         </div>
-        <div ng-if="data.global.type != 'edital'">
+        <div ng-if="data.global.isType === true">
         <h1><i class="fa fa-file-text" aria-hidden="true"></i></span> Oportunidades</h1>
         </div> 
     </div>
 </header>
 
 <div id="lista-dos-oportunidades" class="lista opportunity" infinite-scroll="data.global.filterEntity === 'opportunity' && addMore('opportunity')" ng-show="data.global.filterEntity === 'opportunity'">
-    <div ng-if="data.global.type === 'edital'">
+    <div ng-if="data.global.isType === false">
         <?php $this->part('search/list-edital-item'); ?>
     </div>
-    <div ng-if="data.global.type != 'edital'">
+    <div ng-if="data.global.isType === true">
         <?php $this->part('search/list-opportunity-item'); ?>
     </div>
 </div>
