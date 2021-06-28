c4eadec653cc4e5a668e8e0bad7e18bfa380e1fd
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2021-06-28 11:13:59 -0300

    #134 🐛 corrgindo a condicao de mostrar somente editais e concursos

--- layouts/parts/search/list-opportunity.php
+++ layouts/parts/search/list-opportunity.php
@@ -1,19 +1,20 @@
 <header id="opportunity-list-header" class="entity-list-header clearfix" ng-show="data.global.filterEntity == 'opportunity'">
     <div class="clearfix">
-        <div ng-if="data.global.type === 'edital' || data.global.type === 'concurso'">
-        <h1><i class="fa fa-file-text" aria-hidden="true"></i></span> Editais e Concursos
+        <div ng-if="data.global.type == 'edital'">
+        <h1><i class="fa fa-file-text" aria-hidden="true"></i></span> Editais e Concursos</h1>
+        </br>
         </div>
-        <div ng-if="data.global.type !== 'edital' || data.global.type === 'concurso'">
+        <div ng-if="data.global.type != 'edital'">
         <h1><i class="fa fa-file-text" aria-hidden="true"></i></span> Oportunidades</h1>
         </div> 
     </div>
 </header>
 
 <div id="lista-dos-oportunidades" class="lista opportunity" infinite-scroll="data.global.filterEntity === 'opportunity' && addMore('opportunity')" ng-show="data.global.filterEntity === 'opportunity'">
-    <div ng-if="data.global.type === 'edital' || data.global.type === 'concurso'">
+    <div ng-if="data.global.type === 'edital'">
         <?php $this->part('search/list-edital-item'); ?>
     </div>
-    <div ng-if="data.global.type !== 'edital'|| data.global.type != 'concurso'">
+    <div ng-if="data.global.type != 'edital'">
         <?php $this->part('search/list-opportunity-item'); ?>
     </div>
 </div>
