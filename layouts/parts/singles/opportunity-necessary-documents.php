63377ca978e5ab6c19e665ae43ddad595bb53ee6
ericsonmoreira <moreira.ericson@gmail.com>
2021-08-03 12:42:54 -0300

    add estilização e testando

--- layouts/parts/singles/opportunity-necessary-documents.php
+++ layouts/parts/singles/opportunity-necessary-documents.php
@@ -1,27 +1,49 @@
+<?php
+
+$registrationFieldConfigurationRepo = $app->repo('RegistrationFieldConfiguration');
+
+$registrationFieldConfigurations = $registrationFieldConfigurationRepo->findBy(['owner' => $entity->id]);
+
+?>
+
+<style>
+    #registration {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #registration span {
+        display: flex;
+    }
+
+    #is-required {
+        margin-left: 1rem;
+    }
+
+    #registration-description {
+        font-size: 16px;
+        line-height: 16px;
+        letter-spacing: 0.4px;
+        color: rgba(0, 0, 0, 0.6);
+    }
+</style>
+
 <div id="necessary-documents" class="aba-content">
     <?php $this->applyTemplateHook('tab-space', 'begin'); ?>
-    <h1>Documentos necessários</h1>
-    <hr />
-    <div>
-        <h6>Comprovação de jurado</h6>
-        <h6 class="registration-help">Comprovação de jurado</h6>
-    </div>
-    <hr />
-    <div>
-        <h6>Comprovação de jurado</h6>
-        <h6 class="registration-help">Comprovação de jurado</h6>
-    </div>
-    <hr />
-    <div>
-        <h6>Comprovação de jurado</h6>
-        <h6 class="registration-help">Comprovação de jurado</h6>
-    </div>
-    <hr />
-    <div>
-        <h6>Comprovação de jurado</h6>
-        <h6 class="registration-help">Comprovação de jurado</h6>
-    </div>
-    <hr />
-
+    <h4>Lista de documentos necessários para está inscrição:</h4>
+    <?php foreach ($registrationFieldConfigurations as $registration) : ?>
+        <div id="registration">
+            <span>
+                <strong><?php echo $registration->title ?></strong>
+                <?php if ($registration->required) : ?>
+                    <h6 id="is-required" class="registration-help">Obrigatório</h6>
+                <?php else : ?>
+                    <h6 id="is-required" class="registration-help">Não Obrigatório</h6>
+                <?php endif; ?>
+            </span>
+            <h6 id="registration-description"><?php echo $registration->description ?></h6>
+        </div>
+        <hr />
+    <?php endforeach; ?>
     <?php $this->applyTemplateHook('tab-space', 'end'); ?>
 </div>
