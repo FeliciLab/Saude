bcc37d202fcdfbd47a8c5c77da32d331c8645344
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2021-08-05 17:10:56 -0300

    #162 🚧 Adicionada a aba de isncrições e listando os subprojetos

---
+++ layouts/parts/singles/project-list.php
@@ -0,0 +1,41 @@
+<?php 
+use MapasCulturais\i;
+use MapasCulturais\Entities\Opportunity;
+
+if($this->controller->action === 'create'){ 
+    return;
+}
+
+
+$is_project = false;
+$label = 'Projetos';
+
+if($entity instanceof MapasCulturais\Entities\Project){
+    $is_project = true;
+    $label = 'Subprojetos';
+}
+
+?>
+
+<div class="widget">
+    <?php if ($projects): ?>
+        <?php foreach ($projects as $project): ?>
+    
+            <h2 style="color: black;">
+                <span><?php echo $project->name; ?></span>
+            </h2>
+
+            <span>
+                <?php $this->applyTemplateHook('entity-opportunities','before'); ?>
+
+                <?php $this->applyTemplateHook('entity-opportunities','begin'); ?>
+                <?php foreach($entity->opportunities as $opportunity): ?>
+                <?php $this->part('entity-opportunities--item', ['opportunity' => $opportunity, 'entity' => $entity]) ?>
+                <?php endforeach; ?>
+                <?php $this->applyTemplateHook('entity-opportunities','end'); ?>
+
+                <?php $this->applyTemplateHook('entity-opportunities','after'); ?>
+            </span>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
