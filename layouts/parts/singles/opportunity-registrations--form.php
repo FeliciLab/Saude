24e05f1f2d798cb7cdd0936710bd5fd0bc47ffba
Junior-Shyko <franciscoanto@gmail.com>
2021-08-05 18:25:37 -0300

    #161 📱 - up layout para mobile

--- layouts/parts/singles/opportunity-registrations--form.php
+++ layouts/parts/singles/opportunity-registrations--form.php
@@ -5,20 +5,21 @@ $app = App::i();
 $user = $app->user;
 
 $usuario = $entity->evaluationMethodConfiguration->getUserRelation($user);
+
 if ($entity->isRegistrationOpen()): ?>
     <?php if ($app->auth->isUserAuthenticated()): ?>
-        <?php if(empty($usuario)): //SE CASO ESSE USUÁRIO FOI ALGUM AVALIADOR O FORM NAO APARECERÁ PARA ELE?>
-        <form id="opportunity-registration" class="registration-form clearfix">
-            <p class="registration-help" style="margin-left: -70px;"><?php \MapasCulturais\i::_e("Para iniciar sua inscrição, selecione o agente responsável. Ele deve ser um agente individual (pessoa física), com um CPF válido preenchido.");?></p>
+        <?php if(empty($usuario)): //SE CASO ESSE USUÁRIO FOI ALGUM AVALIADOR O FORM NAO APARECERÁ PARA ELE ?>
+        <form id="opportunity-registration " class="registration-form clearfix">
+            <p class="registration-help white-top" style="font-size: 14px;"><?php \MapasCulturais\i::_e("Para iniciar sua inscrição, selecione o agente responsável. Ele deve ser um agente individual (pessoa física), com um CPF válido preenchido.");?></p>
             <div>
-                <div id="select-registration-owner-button" class="input-text margin-left-70" ng-click="editbox.open('editbox-select-registration-owner', $event)">{{data.registration.owner ? data.registration.owner.name : data.registration.owner_default_label}}</div>
+                <div id="select-registration-owner-button" class="input-text" ng-click="editbox.open('editbox-select-registration-owner', $event)">{{data.registration.owner ? data.registration.owner.name : data.registration.owner_default_label}}</div>
                 <edit-box id="editbox-select-registration-owner" position="top" title="<?php \MapasCulturais\i::esc_attr_e("Selecione o agente responsável pela inscrição.");?>" cancel-label="<?php \MapasCulturais\i::esc_attr_e("Cancelar");?>" close-on-cancel='true' spinner-condition="data.registrationSpinner">
                     <find-entity id='find-entity-registration-owner' entity="agent" no-results-text="<?php \MapasCulturais\i::esc_attr_e("Nenhum agente encontrado");?>" select="setRegistrationOwner" api-query='data.relationApiQuery.owner' spinner-condition="data.registrationSpinner"></find-entity>
                     <strong><?php \MapasCulturais\i::_e("Apenas são visíveis os agentes publicados.");?> <a target="_blank" href="<?php echo $app->createUrl('panel', 'agents') ?>"><?php \MapasCulturais\i::_e("Ver mais.");?></a></strong>
                 </edit-box>
             </div>
             <div>
-                <a class="btn btn-primary" style="color: #ffffff;" ng-click="register()" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Fazer inscrição");?></a>
+                <a class="btn btn-primary btn-register-opportunity" style="color: #ffffff;" ng-click="register()" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Fazer inscrição");?></a>
             </div>
         </form>
         <?php endif; ?>
@@ -36,3 +37,5 @@ if ($entity->isRegistrationOpen()): ?>
 
     <?php endif; ?>
 <?php endif; ?>
+<?php //dump($entity); ?>
+
