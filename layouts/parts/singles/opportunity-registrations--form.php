961298cbf1cffba651d2eb654db1cd42f04b99b6
Junior-Shyko <franciscoanto@gmail.com>
2021-08-11 08:57:01 -0300

    #161 - Ocultando botao  de acessar inscricao

--- layouts/parts/singles/opportunity-registrations--form.php
+++ layouts/parts/singles/opportunity-registrations--form.php
@@ -6,14 +6,16 @@ $app = App::i();
 $user = $app->user;
 
 $usuario = $entity->evaluationMethodConfiguration->getUserRelation($user);
-dump($entity->singleUrl);
-$btnHideShow = false;
-if ($entity instanceof \MapasCulturais\Entities\Opportunity) {
-    echo "Oportunidade";
-} else {
-    echo "Nao é oportunidade";
-}
 
+$url_atual = $app->getView()->getController()->id;
+
+$btnHideShow = false;
+
+if (strpos($url_atual, 'opportunity') !== false) {
+    $btnHideShow = true;
+} else {
+    $btnHideShow = false;
+}
 
 if ($entity->isRegistrationOpen()) : ?>
     <?php if ($app->auth->isUserAuthenticated()) : ?>
@@ -30,25 +32,10 @@ if ($entity->isRegistrationOpen()) : ?>
                         </edit-box>
                         <a class="btn btn-primary btn-register-opportunity" style="color: #ffffff;" ng-click="register(<?php echo $entity->id; ?>)" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Fazer inscrição"); ?></a>
                     </div>
-                    <div>
+                    <div style="visibility: <?php echo $btnHideShow ? 'hidden' : 'visible' ?>">
                         <a href="<?= $entity->singleUrl; ?>" class="btn btn-access-opportunity" style="color: #ffffff;" rel='noopener noreferrer' title="Acessar inscrições"><?php \MapasCulturais\i::_e("Acessar Inscrição"); ?></a>
                     </div>  
                 </div>
-         
-                
-                
-                <!-- <div>
-                    <div id="select-registration-owner-button" class="input-text" ng-click="editbox.open('editbox-select-registration-owner', $event)">{{data.registration.owner ? data.registration.owner.name : data.registration.owner_default_label}}</div>
-                    <edit-box id="editbox-select-registration-owner" position="top" title="<?php \MapasCulturais\i::esc_attr_e("Selecione o agente responsável pela inscrição."); ?>" cancel-label="<?php \MapasCulturais\i::esc_attr_e("Cancelar"); ?>" close-on-cancel='true' spinner-condition="data.registrationSpinner">
-                        <find-entity id='find-entity-registration-owner' entity="agent" no-results-text="<?php \MapasCulturais\i::esc_attr_e("Nenhum agente encontrado"); ?>" select="setRegistrationOwner" api-query='data.relationApiQuery.owner' spinner-condition="data.registrationSpinner"></find-entity>
-                        <strong><?php \MapasCulturais\i::_e("Apenas são visíveis os agentes publicados."); ?> <a target="_blank" href="<?php echo $app->createUrl('panel', 'agents') ?>"><?php \MapasCulturais\i::_e("Ver mais."); ?></a></strong>
-                    </edit-box>
-                </div> -->
-                <!-- <div>
-                        <a class="btn btn-primary btn-register-opportunity" style="color: #ffffff;" ng-click="register(<?php echo $entity->id; ?>)" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Fazer inscrição"); ?></a>
-                        <a href="<?= $entity->singleUrl; ?>" class="btn btn-access-opportunity" style="color: #ffffff;" rel='noopener noreferrer' title="Acessar inscrições"><?php \MapasCulturais\i::_e("Acessar Inscrição"); ?></a>
-                    </div> -->
-
             </form>
         <?php endif; ?>
     <?php else : ?>
