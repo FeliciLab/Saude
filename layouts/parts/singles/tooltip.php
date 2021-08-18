b08e3b1fb06717f90e49f0c69aa25fc3bc1d68e7
ericsonmoreira <moreira.ericson@gmail.com>
2021-08-18 11:38:34 -0300

    ajustes de layout em tooltip single #441

--- layouts/parts/singles/tooltip.php
+++ layouts/parts/singles/tooltip.php
@@ -1,1 +1,6 @@
-<p title="<?php echo $title; ?>"><?php echo $chield; ?></p>
+<!-- 
+  Props: 
+    - title: string com título do tooltip
+    - chield: string conteúdo do tooltip
+ -->
+<p class="hltip" title="<?php echo $title; ?>"><?php echo $chield; ?></p>
