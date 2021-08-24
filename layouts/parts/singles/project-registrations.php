3d19ec8d4840dc7db5c92215e75cb7fc9f1ba7d0
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2021-08-24 11:38:20 -0300

    #162 ✨ adicionando oportunidades em projetos e subprojetos

--- layouts/parts/singles/project-registrations.php
+++ layouts/parts/singles/project-registrations.php
@@ -3,7 +3,7 @@
 use MapasCulturais\i;
 use MapasCulturais\Entities\Opportunity;
 
-if($this->controller->action === 'create'){ 
+if ($this->controller->action === 'create') {
     return;
 }
 
@@ -14,20 +14,15 @@ $parent = $entity->parent;
 ?>
 
 <div id="inscricoes" class="aba-content">
-<?php if ($parent != null): ?>
 
-    <?php foreach($entity->getOpportunities(Opportunity::STATUS_DRAFT) as $opportunity): ?>
+    <?php foreach ($entity->getOpportunities(Opportunity::STATUS_DRAFT) as $opportunity) : ?>
         <?php $this->part('entity-opportunities--item', ['opportunity' => $opportunity, 'entity' => $entity]) ?>
         <br>
     <?php endforeach; ?>
-    
-    <?php else: ?>
 
-        <?php $this->applyTemplateHook('tab-inscricoes', 'begin'); ?>
-        <div id="inscricoes">
-            <?php $this->part('singles/project-list', ['entity' => $entity, 'projects' => $entity->children->toArray()]); ?>
-        </div>
-        <?php $this->applyTemplateHook('tab-inscricoes', 'end'); ?>
-
-    <?php endif ?>
+    <?php $this->applyTemplateHook('tab-inscricoes', 'begin'); ?>
+    <div id="inscricoes">
+        <?php $this->part('singles/project-list', ['entity' => $entity, 'projects' => $entity->children->toArray()]); ?>
+    </div>
+    <?php $this->applyTemplateHook('tab-inscricoes', 'end'); ?>
 </div>
