f54e478b13ebfe5671dd1e266f3bc2441c83fa23
ericsonmoreira <moreira.ericson@gmail.com>
2021-08-27 09:25:03 -0300

    adjusts to PR #432

--- layouts/parts/singles/opportunity-registrations--form.php
+++ layouts/parts/singles/opportunity-registrations--form.php
@@ -2,7 +2,7 @@
 
 use MapasCulturais\App;
 
-$url_atual = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url_atual = $app->view->controller->id;
 
 $app = App::i();
 
@@ -12,12 +12,7 @@ $userRelation = $entity->evaluationMethodConfiguration->getUserRelation($user);
 
 $btnHideShow = false;
 
-function isOwnsOfEntity($user, $entity)
-{
-    return $user->id == $entity->owner->userId;
-}
-
-if (strpos($url_atual, 'oportunidade') !== false) {
+if (strpos($url_atual, 'opportunity') !== false) {
     $btnHideShow = true;
 } else {
     $btnHideShow = false;
@@ -27,7 +22,7 @@ if ($entity->isRegistrationOpen()) : ?>
     <?php if ($app->auth->isUserAuthenticated()) : ?>
 
         <!-- // CASO ESSE USUÁRIO FOI ALGUM AVALIADOR ou for o dono da entidade, então O FORM NAO APARECERÁ PARA ELE  -->
-        <?php if (empty($userRelation) && !isOwnsOfEntity($user, $entity)) : ?>
+        <?php if (empty($userRelation) && !($user->id == $entity->owner->userId)) : ?>
             <form class="registration-form clearfix">
                 <p class="registration-help white-top" style="font-size: 14px;"><?php \MapasCulturais\i::_e("Para iniciar sua inscrição, selecione o agente responsável. Ele deve ser um agente individual (pessoa física), com um CPF válido preenchido."); ?></p>
                 <div class="registration-form-content">
