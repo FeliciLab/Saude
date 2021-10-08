5480e78e6710d0ac76fd69dfeda65a26468c0322
Fernanda Nascimento <44043740+FernandaNascimento26@users.noreply.github.com>
2021-10-08 14:36:14 -0300

    215 - Modificar a posição do botão Salvar" na página de inscrição (#228)
    * 215- 🚧 chamando hook do botao no tema e tambem o estilo para esconder o antigo botao
    
    * 215- 💄 adicionando estilo para o novo botao de salvar inscricao para depois
    
    * 215- 🚧 adicionando arquivo com o novo botao de salvar para depois
    
    * 215- 🚧 adicionando um css apenas para ser chamado no hook e desabilitar o botao de salvar no canto superior

---
+++ layouts/parts/singles/button/registration-save--button.php
@@ -0,0 +1,5 @@
+<div class="registration-button-save">
+<?php ($this->controller->id === 'registration') ?>
+    <a class="btn btn-default js-submit-button" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Salvar para depois");?></a>
+</div>
+
