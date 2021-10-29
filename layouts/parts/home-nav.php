a2d8090817b1e09b9a919efe1315303116388ac3
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2021-10-29 11:48:27 -0300

    🎨 #246 suprimindo icone  de oportunidades na home

--- layouts/parts/home-nav.php
+++ layouts/parts/home-nav.php
@@ -20,12 +20,12 @@
             </li>
         <?php endif; ?>
 
-        <?php if($app->isEnabled('opportunities')): ?>
+        <?php /*if($app->isEnabled('opportunities')): ?>
             <li id="nav-opportunities">
                 <a class="icon icon-opportunity" href="#home-opportunities"></a>
                 <span class="nav-title"><?php $this->dict('entities: Opportunities') ?></span>
             </li>
-        <?php endif; ?>
+        <?php endif; */?>
 
         <li id="nav-developers">
             <a class="icon icon-developers" href="#home-developers"></a>
