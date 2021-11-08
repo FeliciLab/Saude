1f4e226fd8850f72cfc236aadcebd428c63929c7
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2021-11-08 08:49:32 -0300

    Atualizacao dos textos da página principal do Mapa da Saúde (#247) (#252)
    * 🎨 #246 adicionando textos atualizados para a home
    
    * 🎨 #246 suprimindo o article de oportunidades na home
    
    * 🎨 #246 suprimindo icone  de oportunidades na home
    
    * 🎨 #246 adicionando estilo aos textos da Home
    
    * #246 ajustando textos apos QA
    
    * 246 alterando o termo developers para inovacao aberta e colaborativa
    
    * 246 alterando o termo developers para Colabore
    
    Co-authored-by: Fernanda Nascimento <44043740+FernandaNascimento26@users.noreply.github.com>

--- layouts/parts/home-nav.php
+++ layouts/parts/home-nav.php
@@ -20,16 +20,16 @@
             </li>
         <?php endif; ?>
 
-        <?php if($app->isEnabled('opportunities')): ?>
+        <?php /*if($app->isEnabled('opportunities')): ?>
             <li id="nav-opportunities">
                 <a class="icon icon-opportunity" href="#home-opportunities"></a>
                 <span class="nav-title"><?php $this->dict('entities: Opportunities') ?></span>
             </li>
-        <?php endif; ?>
+        <?php endif; */?>
 
         <li id="nav-developers">
             <a class="icon icon-developers" href="#home-developers"></a>
-            <span class="nav-title"><?php \MapasCulturais\i::_e("Desenvolvedores");?></span>
+            <span class="nav-title"><?php \MapasCulturais\i::_e("Colabore");?></span>
         </li>
         <li><a class="down icon icon-select-arrow" href="#"></a></li>
     </ul>
