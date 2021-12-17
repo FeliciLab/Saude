92c2dddd5cd44228acbc0fb729ae715437922da4
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2021-12-16 22:51:04 -0300

    adicionando modal

---
+++ layouts/parts/modals/open-modal-confirm-subscribe.php
@@ -0,0 +1,28 @@
+<div class="remodal" data-remodal-id="modal-info-inscricao">
+
+    <button data-remodal-action="close" class="remodal-close" style="float:right;"></button>
+
+
+<span></span>
+    <h3>Você está enviando sua inscrição para análise</h3>
+<div>
+<h4 style="color: #F26822; font-weight: bold;">Você conferiu os seus dados?</h4>
+</div>
+
+<div>
+
+<p>Ao confirmar, essa <b>ação não poderá ser desfeita</b><br>
+e você não conseguirá editar os dados. 
+</p>
+
+<p>
+Você poderá verificar o status de sua inscrição em <br>
+<b>Meu Perfil > Minhas Inscrições. </b>
+</p>
+
+</div>
+        <br>
+        <button data-remodal-action="cancel" class="btn btn-default" title="Sair da resposta"> Voltar</button>
+        <a class="btn btn-primary" ng-click="sendRegistration()" rel='noopener noreferrer'><?php \MapasCulturais\i::_e("Confirmar");?></a>
+    </form>
+</div>
