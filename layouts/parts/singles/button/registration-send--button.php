64aa2eee700ab770d12eb3f453decc1a34cda7ef
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2021-12-21 09:15:38 -0300

    Main to develop (#301)
    * Atualiza link recuperação dinâmica keycloak env
    
    * Lista somente projetos, subprojetos e oportunidades habilitadas
    
    * removendo comentários desnecessários
    
    * Não mostra projetos, subprojetos e oportunidades em rascunho ou arquivadas
    
    * #292- adicionando redesign na visao do usuario nao logado
    
    * Avaliaçao tecnica aplica resultado (selecionado e nao selecionado) somente para inscriçoes enviadas
    
    * resolve Exception todos selecionados na avaliaçao tecnica
    
    * #592 trazendo o arquivo owner.php para o tema e alterando o css
    
    * #592 alterando estilo do botao
    
    * #592 - removendo o arquivo owner.php e adicionando o estilo ao id
    
    * changes at resource about new evaluation
    
    * Adicionando label obrigatório seguindo o padrão do mapas
    
    * remove consoles logs e remove ultima mensagem vazia ao salvar
    
    * Tratando mensagem unknow errors
    
    * Feature/limite inscricoes projetos (#296)
    
    * nova parts para projeto registrationLimitPerOwnerProject
    
    * Implementa novo campo limite e validação hook registration para projeto
    
    * apply hook edit and create project
    
    * add section in field and change hook tab opportunity
    
    * remove  refactor
    
    * mostra uma mensagem por requisiçao
    
    * feat: adicionado a correcao para quando houver mais de uma opp dentro do mesmo projeto (#299)
    
    * #594 alteracoes hierarquia visual do avatar de perfil do agente (#298)
    
    * #594 - adicionando novo avatar agente e modificando nomenclaturas
    
    * #594- adicionando estilizacao do avatar e do texto
    
    * add validaçao plugin tecnica NA
    
    * Pop-up de confirmação de envio de inscrição (#300)
    
    * adicionando modal
    
    * #591 - adicionando modal
    
    * Formatando e fechando modal de confirmação
    
    * #591-removendo sobrescrita e chamando hooks
    
    Co-authored-by: victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
    
    Co-authored-by: Rafa Chaves <rafael.chaves.freitas@gmail.com>
    Co-authored-by: FernandaNascimento26 <nandinha.cefet@hotmail.com>
    Co-authored-by: Pedro <lspedrovitor@gmail.com>
    Co-authored-by: pedrovitor074 <38664500+pedrovitor074@users.noreply.github.com>
    Co-authored-by: Fernanda Nascimento <44043740+FernandaNascimento26@users.noreply.github.com>

---
+++ layouts/parts/singles/button/registration-send--button.php
@@ -0,0 +1,5 @@
+<a data-remodal-target="modal-info-registration-confirm" class="btn btn-primary" onclick=""> Enviar Inscrição</a>
+
+
+<!-- modal de Confirmação de inscrição -->
+<?php $this->part('modals/open-modal-confirm-subscribe') ?>
