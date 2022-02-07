8ac3c31cd2dfacbccecd3319d2747fbc1484ef36
Junior-Shyko <franciscoanto@gmail.com>
2022-02-07 15:29:59 -0300

    🔥 - removido send-button

--- layouts/parts/singles/button/registration-send--button.php
+++
@@ -1,5 +0,0 @@
-<a data-remodal-target="modal-info-registration-confirm" class="btn btn-primary" onclick=""> Enviar Inscrição</a>
-
-
-<!-- modal de Confirmação de inscrição -->
-<?php $this->part('modals/open-modal-confirm-subscribe') ?>
