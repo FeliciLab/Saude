1d4c8f4227cc35a87702387435344feaeff7fc82
Pedro <lspedrovitor@gmail.com>
2022-02-14 11:41:28 -0300

    #630 adicionado botão para ediçao de incricao

--- layouts/parts/singles/opportunity-registrations--user-registrations.php
+++ layouts/parts/singles/opportunity-registrations--user-registrations.php
@@ -20,105 +20,109 @@ if (!empty($registrations)) {
 
 </style>
 <?php if ($registrations) : ?>
-    <table class="my-registrations" style="width: 100%">
-        <caption class="caption-table"><?php \MapasCulturais\i::_e("Minhas inscrições"); ?></caption>
-        <thead>
-            <tr>
-                <th class="registration-status-col" style="text-align: center;width: 30%">
-                    <?php \MapasCulturais\i::_e("Inscrição"); ?>
-                </th>
-                <th class="registration-agents-col" style="text-align: center;">
-                    <?php \MapasCulturais\i::_e("Agentes"); ?>
-                </th>
-                <th class="registration-status-col" style="text-align: left; text-align: center;">
-                    <?php \MapasCulturais\i::_e("Data de Envio"); ?>
-                </th>
-                <?php if (
-                    $verifyPublish->publishedRegistrations == true
-                    && $typeEvaluation[0]->type->id == 'technical'
-                ) : ?>
-                    <th class="registration-status-col" style="text-align: center;">
-                        <?php \MapasCulturais\i::_e("Nota preliminar"); ?>
-                    </th>
-                    <th class="registration-status-col" style="text-align: center; width: 20%;">
-                        <?php \MapasCulturais\i::_e("Nota final"); ?>
-                    </th>
-                <?php endif; ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($registrations as $registration) :
-                $reg_args = ['registration' => $registration, 'opportunity' => $entity];
-            ?>
+    <div style="overflow-x:auto;">
+        <table class="my-registrations" style="width: 100%">
+            <caption class="caption-table"><?php \MapasCulturais\i::_e("Minhas inscrições"); ?></caption>
+            <thead>
                 <tr>
-                    <?php $this->applyTemplateHook('user-registration-table--registration', 'begin', $reg_args); ?>
-                    <td class="registration-status-col" style="text-align: center;">
-                        <?php $this->applyTemplateHook('user-registration-table--registration--number', 'begin', $reg_args); ?>
-                        <a href="<?php echo $registration->singleUrl ?>"><?php echo $registration->number ?> </a>
-                        <?php if($registration->status == Registration::STATUS_DRAFT){ ?> 
-                            <p class='text-danger'><?php \MapasCulturais\i::_e("Inscrição em rascunho"); ?></p>
-                        <?php } ?>
-                        <?php $this->applyTemplateHook('user-registration-table--registration--number', 'end', $reg_args); ?>
-                    </td>
-                    <td class="registration-agents-col" style="text-align: center;">
-                        <?php $this->applyTemplateHook('user-registration-table--registration--agents', 'begin', $reg_args); ?>
-                        <p>
-                            <span class="label"><?php \MapasCulturais\i::_e("Responsável"); ?></span><br>
-                            <?php echo htmlentities($registration->owner->name); ?>
-                        </p>
-                        <?php
-                        foreach ($app->getRegisteredRegistrationAgentRelations() as $def) :
-                            if (!$entity->useRegistrationAgentRelation($def))
-                                continue;
-                        ?>
-                            <?php if ($agents = $registration->getRelatedAgents($def->agentRelationGroupName)) : ?>
-                                <p>
-                                    <span class="label"><?php echo $def->label ?></span><br>
-                                    <?php echo htmlentities($agents[0]->name); ?>
-                                </p>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                        <?php $this->applyTemplateHook('user-registration-table--registration--agents', 'end', $reg_args); ?>
-                    </td>
-                    <td class="registration-status-col" style="text-align: center; vertical-align: middle;">
-                        <?php $this->applyTemplateHook('user-registration-table--registration--status', 'begin', $reg_args); ?>
-                        <?php if ($registration->status > 0) : ?>
-                            <?php echo $registration->sentTimestamp ? $registration->sentTimestamp->format(\MapasCulturais\i::__('d/m/Y à\s H:i')) : ''; ?>.
-                            <div style="display: flex">
-                                <a class="btn btn-small btn-primary mt-auto" href="<?php echo $registration->singleUrl ?>"><?php \MapasCulturais\i::_e("Editar inscrição"); ?></a>
-                                <a class="btn btn-small btn-primary mt-auto" style="margin-left: 10px" href="<?php echo $registration->singleUrl ?>"><?php \MapasCulturais\i::_e("Ver comprovante"); ?></a>
-                            </div>
-                        <?php else : ?>
-                            <a class="btn btn-small btn-primary mt-auto" href="<?php echo $registration->singleUrl ?>"><?php \MapasCulturais\i::_e("Editar inscrição"); ?></a>
-                        <?php endif; ?>
-                        <?php $this->applyTemplateHook('user-registration-table--registration--status', 'end', $reg_args); ?>
-                    </td>
+                    <th class="registration-status-col" style="text-align: center;width: 30%">
+                        <?php \MapasCulturais\i::_e("Inscrição"); ?>
+                    </th>
+                    <th class="registration-agents-col" style="text-align: center;">
+                        <?php \MapasCulturais\i::_e("Agentes"); ?>
+                    </th>
+                    <th class="registration-status-col" style="text-align: left; text-align: center;">
+                        <?php \MapasCulturais\i::_e("Data de Envio"); ?>
+                    </th>
                     <?php if (
                         $verifyPublish->publishedRegistrations == true
                         && $typeEvaluation[0]->type->id == 'technical'
                     ) : ?>
-                        <td>
-                            <?php echo $registration->preliminaryResult; ?>
-                        </td>
-
-                        <?php
-                        //ENTROU COM RECURSO E JA FOI PUBLICADO
-                        if ($resource['text'] !== "" && $resource['publish'] == true) {
-                            echo '<td>' . $registration->consolidatedResult . '</td>';
-                        } else
-                            //SE ENTROU EM RECURSO MAS AINDA NAO FOI PUBLICADO
-                            if ($resource['text'] !== "" && $resource['publish'] == false) {
-                                echo '<td>Recurso enviado. Aguarde!</td>';
-                            } else
-                                //SE NÃO ENTROU EM RECURSO.
-                                if ($resource['text'] == 'Não existe texto' && $resource['publish'] == 'sem publicacao') {
-                                    echo '<td>' . $registration->consolidatedResult . '</td>';
-                                }
-                        ?>
+                        <th class="registration-status-col" style="text-align: center;">
+                            <?php \MapasCulturais\i::_e("Nota preliminar"); ?>
+                        </th>
+                        <th class="registration-status-col" style="text-align: center; width: 20%;">
+                            <?php \MapasCulturais\i::_e("Nota final"); ?>
+                        </th>
                     <?php endif; ?>
-                    <?php $this->applyTemplateHook('user-registration-table--registration', 'end', $reg_args); ?>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($registrations as $registration) :
+                    $reg_args = ['registration' => $registration, 'opportunity' => $entity];
+                ?>
+                    <tr>
+                        <?php $this->applyTemplateHook('user-registration-table--registration', 'begin', $reg_args); ?>
+                        <td class="registration-status-col" style="text-align: center;">
+                            <?php $this->applyTemplateHook('user-registration-table--registration--number', 'begin', $reg_args); ?>
+                            <a href="<?php echo $registration->singleUrl ?>"><?php echo $registration->number ?> </a>
+                            <?php if($registration->status == Registration::STATUS_DRAFT){ ?> 
+                                <p class='text-danger'><?php \MapasCulturais\i::_e("Inscrição em rascunho"); ?></p>
+                            <?php } ?>
+                            <?php $this->applyTemplateHook('user-registration-table--registration--number', 'end', $reg_args); ?>
+                        </td>
+                        <td class="registration-agents-col" style="text-align: center;">
+                            <?php $this->applyTemplateHook('user-registration-table--registration--agents', 'begin', $reg_args); ?>
+                            <p>
+                                <span class="label"><?php \MapasCulturais\i::_e("Responsável"); ?></span><br>
+                                <?php echo htmlentities($registration->owner->name); ?>
+                            </p>
+                            <?php
+                            foreach ($app->getRegisteredRegistrationAgentRelations() as $def) :
+                                if (!$entity->useRegistrationAgentRelation($def))
+                                    continue;
+                            ?>
+                                <?php if ($agents = $registration->getRelatedAgents($def->agentRelationGroupName)) : ?>
+                                    <p>
+                                        <span class="label"><?php echo $def->label ?></span><br>
+                                        <?php echo htmlentities($agents[0]->name); ?>
+                                    </p>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                            <?php $this->applyTemplateHook('user-registration-table--registration--agents', 'end', $reg_args); ?>
+                        </td>
+                        <td class="registration-status-col" style="text-align: center; vertical-align: middle;">
+                            <?php $this->applyTemplateHook('user-registration-table--registration--status', 'begin', $reg_args); ?>
+                            <?php if ($registration->status > 0) : ?>
+                                <?php echo $registration->sentTimestamp ? $registration->sentTimestamp->format(\MapasCulturais\i::__('d/m/Y à\s H:i')) : ''; ?>.
+                                <div class="div-to-show-button">
+                                    <form action="<?php echo $app->createUrl('registration', 'alterStatusRegistration', [$registration->id]); ?>" method="post">
+                                        <button type="submit" class="btn btn-small btn-primary mt-auto"  style="width: 113.81px;" href="<?php echo $registration->singleUrl ?>"><?php \MapasCulturais\i::_e("Editar inscrição"); ?></button>
+                                    </form>
+                                    <a class="btn btn-small btn-primary mt-auto button-ver" href="<?php echo $registration->singleUrl ?>"><?php \MapasCulturais\i::_e("Ver comprovante"); ?></a>
+                                </div>
+                            <?php else : ?>
+                                <a class="btn btn-small btn-primary mt-auto" style="width: 113.81px;" href="<?php echo $registration->singleUrl ?>"><?php \MapasCulturais\i::_e("Editar inscrição"); ?></a>
+                            <?php endif; ?>
+                            <?php $this->applyTemplateHook('user-registration-table--registration--status', 'end', $reg_args); ?>
+                        </td>
+                        <?php if (
+                            $verifyPublish->publishedRegistrations == true
+                            && $typeEvaluation[0]->type->id == 'technical'
+                        ) : ?>
+                            <td>
+                                <?php echo $registration->preliminaryResult; ?>
+                            </td>
+
+                            <?php
+                            //ENTROU COM RECURSO E JA FOI PUBLICADO
+                            if ($resource['text'] !== "" && $resource['publish'] == true) {
+                                echo '<td>' . $registration->consolidatedResult . '</td>';
+                            } else
+                                //SE ENTROU EM RECURSO MAS AINDA NAO FOI PUBLICADO
+                                if ($resource['text'] !== "" && $resource['publish'] == false) {
+                                    echo '<td>Recurso enviado. Aguarde!</td>';
+                                } else
+                                    //SE NÃO ENTROU EM RECURSO.
+                                    if ($resource['text'] == 'Não existe texto' && $resource['publish'] == 'sem publicacao') {
+                                        echo '<td>' . $registration->consolidatedResult . '</td>';
+                                    }
+                            ?>
+                        <?php endif; ?>
+                        <?php $this->applyTemplateHook('user-registration-table--registration', 'end', $reg_args); ?>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 <?php endif; ?>
