e356e20203906b2d9c07815dd2b844cac6f2ba5e
Pedro <lspedrovitor@gmail.com>
2022-03-09 14:01:38 -0300

    correcao para esconder o erro de href

--- assets/js/opportunity-phase-button.js
+++ assets/js/opportunity-phase-button.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
    let li = $("div[class='opportunity-phases clear']").children().children();
-   let href_value = $("div[class='opportunity-phases clear']").children().children().children()[0].href;
-   if(href_value){
-        li.css("display", "flex");
-        li.prepend(`<button class="btn-access" style="float: left;" > <i class="fa fa-check-circle-o" aria-hidden="true"></i><a style="color: #fff;text-decoration: none;" href="${href_value}" >Acessar</a></button>`);
+   if(li.length){
+      let href_value = $("div[class='opportunity-phases clear']").children().children().children()[0].href;
+      if(href_value){
+           li.css("display", "flex");
+           li.prepend(`<button class="btn-access" style="float: left;" > <i class="fa fa-check-circle-o" aria-hidden="true"></i><a style="color: #fff;text-decoration: none;" href="${href_value}" >Acessar</a></button>`);
+      }
    }
 });
