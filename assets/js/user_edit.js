c0d805e285d9c3def4307ccf58fe26c24cc5b402
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-04-27 16:28:33 -0300

    adiciona máscara no documento do agente

--- assets/js/user_edit.js
+++ assets/js/user_edit.js
@@ -4,4 +4,19 @@ $(document).ready(function () {
     if(cpf_value.match( /(^\d{3}\d{3}\d{3}\d{2}$)|(^\d{2}\d{3}\d{3}\d{4}\d{2}$)/)){
         cpf.editable('toggleDisabled');
     }
+
+    if(cpf_value){
+        $('#user_cpf').mask(cpf_value.length > 11 ? '00.000.000/0000-00' : '000.000.000-00', {
+            reverse: true
+        });
+    }
+    $('.privado').on('keydown', 'input', function(e) {
+        if($(this)[0].placeholder == "CPF ou CNPJ"){
+            if($(this).val().length > 14){
+                $(this).mask('00.000.000/0000-00')
+            }else{
+                $(this).mask('000.000.000-009');
+            }
+        }
+    });
 });
