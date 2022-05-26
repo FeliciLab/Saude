be9339a97030626a2654ff7ca0a7850560e34e37
Victor Magalhaes <victor.magalhaesp@gmail.com>
2022-05-26 09:11:58 -0300

    atualiza campo cpf mascara

--- assets/js/user_edit.js
+++ assets/js/user_edit.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+
+    let cpf = $("#user_cpf");
+    let cpf_value = cpf.text().replace(/\D/g, "");
+    if(cpf_value.match( /(^\d{3}\d{3}\d{3}\d{2}$)|(^\d{2}\d{3}\d{3}\d{4}\d{2}$)/)){
+        cpf.editable('toggleDisabled');
+    }
+
+
     if(cpf_value){
         $('#user_cpf').mask(cpf_value.length > 11 ? '00.000.000/0000-00' : '000.000.000-00', {
             reverse: true
