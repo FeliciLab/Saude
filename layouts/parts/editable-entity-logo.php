97ba13add103f43cda065b860b9edbb1ca5e1178
andersondantas81 <andersondantas81@gmail.com>
2022-07-25 12:16:55 -0300

    Alteração das logos (Mapa da saúde e ESP)

--- layouts/parts/editable-entity-logo.php
+++ layouts/parts/editable-entity-logo.php
@@ -1,1 +1,1 @@
-<h1 id="small-brand"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-saude.png'); ?>" /></a></h1>
+<h1 id="small-brand"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-mapadasaude.png'); ?>" /></a></h1>
