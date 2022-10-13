a5f153ba7d8b4fd94450e8a9e2c47b81ef1c5d1f
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2022-10-13 09:43:11 -0300

    Develop to main (#366)
    * Atualiza mensagem preview dos e-mails
    
    * Modificação do texto do botão de mudar o status dos inscritos pendentes para selecionados
    
    * Botão para atualizar o CNES
    
    Co-authored-by: Jefferson Oliveira <jeff.doliveira1@outlook.com>
    Co-authored-by: andersondantas81 <andersondantas81@gmail.com>

---
+++ layouts/parts/panel/nav-atualizar-cnes.php
@@ -0,0 +1,6 @@
+<li>
+    <a href="<?php echo $app->createUrl('cnes-integration/estabelecimentos')  ?>" title="Atualizar dados CNES">
+        <i class="fa fa-rotate-right" aria-hidden="true" style="margin-right: 5px;"></i>
+        <?php \MapasCulturais\i::_e("Atualizar dados CNES"); ?>
+    </a>
+</li>
