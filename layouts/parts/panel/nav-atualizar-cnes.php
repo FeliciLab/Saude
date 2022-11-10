81a316a4a8f5478e957f0320437e1956fc43fd6f
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-11-10 10:01:27 -0300

    Remmove nav ação atualizar CNES

--- layouts/parts/panel/nav-atualizar-cnes.php
+++
@@ -1,6 +0,0 @@
-<li>
-    <a href="<?php echo $app->createUrl('cnes-integration/estabelecimentos')  ?>" title="Atualizar dados CNES">
-        <i class="fa fa-rotate-right" aria-hidden="true" style="margin-right: 5px;"></i>
-        <?php \MapasCulturais\i::_e("Atualizar dados CNES"); ?>
-    </a>
-</li>
