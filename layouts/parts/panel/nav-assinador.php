a28b2d38573a535227e3e94e640e0096f368ee9f
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2022-11-24 20:24:39 -0300

    Link no Painel de Conteole para o Assinador

---
+++ layouts/parts/panel/nav-assinador.php
@@ -0,0 +1,6 @@
+<li>
+    <a href="https://assinador.dev.org.br/" target="_blank">
+        <i class="fas fa-file-signature" aria-hidden="true" style="margin-right: 5px;"></i>
+        <?php \MapasCulturais\i::_e("Assinador"); ?>
+    </a>
+</li>
